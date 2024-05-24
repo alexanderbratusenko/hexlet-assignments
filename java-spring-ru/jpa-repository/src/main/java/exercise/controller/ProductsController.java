cd9aa5131481df1aee2136f66543b843134df6ca
@hexlet/cli <support@hexlet.io>
2024-05-24 10:21:12 +0300

    submit java-spring-ru/jpa-repository

---
+++ java-spring-ru/jpa-repository/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,42 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
+import exercise.model.Product;
+import exercise.repository.ProductRepository;
+import exercise.exception.ResourceNotFoundException;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts(@RequestParam(value = "min", required = false) Integer min,
+                                        @RequestParam(value = "max", required = false) Integer max) {
+        return productRepository.findAllByPriceLessThanAndPriceGreaterThanOrderByPrice(
+                Optional.ofNullable(max).orElse(Integer.MAX_VALUE),
+                Optional.ofNullable(min).orElse(Integer.MIN_VALUE)
+        );
+    }
+
+    @GetMapping(path = "/{id}")
+    public Product show(@PathVariable long id) {
+
+        var product =  productRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+
+        return product;
+    }
+}
